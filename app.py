a7583c181a8e5f67127400a14714511cef23d412
Legion <semo4kazai4ik@yandex.ru>
2022-10-02 18:44:36 +0600

    add views director(s), genre(s) add methods put, delete, post for @movies_ns.route

--- app.py
+++ app.py
@@ -12,10 +12,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['RESTX_JSON'] = {'ensure_ascii': False, 'indent': 2}
 db = SQLAlchemy(app)
 api = Api(app)
+
 movies_ns = api.namespace("movies")
+directors_ns = api.namespace("directors")
+genres_ns = api.namespace("genres")
 
 movies_schema = schema.Movie(many=True)
 movie_schema = schema.Movie()
+
+directors_schema = schema.Director(many=True)
+director_schema = schema.Director()
+
+genres_schema = schema.Genre(many=True)
+genre_schema = schema.Genre()
 
 
 class Movie(db.Model):
@@ -75,6 +84,70 @@ class MovieViewId(Resource):
             return {}, 404
 
         return movie_schema.dump(movie), 200
+
+    def put(self, mid):
+        updated_row = db.session.query(Movie).filter(Movie.id == mid).update(request.json)
+
+        if updated_row != 1:
+            return None, 400
+
+        db.session.commit()
+
+        return None, 204
+
+    def delete(self, mid):
+        deleted_row = db.session.query(Movie).filter(Movie.id == mid).delete()
+
+        if deleted_row != 1:
+            return None, 400
+
+        db.session.commit()
+
+        return None, 200
+
+    def post(self):
+        new_row = Movie(**request.json)
+        with db.session.begin():
+            db.session.add(new_row)
+        return "", 201
+
+
+@directors_ns.route("/")
+class DirectorsView(Resource):
+    def get(self):
+        directors = db.session.query(Director).all()
+
+        return directors_schema.dump(directors), 200
+
+
+@directors_ns.route("/<int:did>")
+class DirectorViewId(Resource):
+    def get(self, did):
+        director = db.session.query(Director).get(did)
+
+        if director is None:
+            return {}, 404
+
+        return director_schema.dump(director), 200
+
+
+@genres_ns.route("/")
+class GenresView(Resource):
+    def get(self):
+        genres = db.session.query(Genre).all()
+
+        return genres_schema.dump(genres), 200
+
+
+@genres_ns.route("/<int:gid>")
+class GenreViewId(Resource):
+    def get(self, gid):
+        genre = db.session.query(Genre).get(gid)
+
+        if genre is None:
+            return {}, 404
+
+        return genre_schema.dump(genre), 200
 
 
 if __name__ == '__main__':
